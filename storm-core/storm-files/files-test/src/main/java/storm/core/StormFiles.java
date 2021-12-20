3ae966b09ee537b058f86de852c00c6a7fe32c56
Wesley <tench265@gmail.com>
2021-12-19 23:36:22 -0500

    Moved files to test project

--- storm-core/storm-files/files-test/src/main/java/storm/core/StormFiles.java
+++ storm-core/storm-files/files-test/src/main/java/storm/core/StormFiles.java
@@ -1,9 +1,30 @@
 package storm.core;
 
-public class StormFiles {
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+import storm.core.yaml.StormConfig;
+import storm.core.yaml.StormFileManager;
 
-    public static void main(String[] args) {
+import java.util.function.Consumer;
 
+public class StormFiles extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        new StormFileManager(this).loadAllConfigs();
+
+        Test.getFileConfiguration().getString("");
     }
 
+    public class Test extends StormConfig {
+
+        /**
+         * @param fileName This is for the files name
+         * @param path     This is the path for the config to laod to
+         * @param writer   This is what will be written to it
+         */
+        protected Test(String fileName, String path, Consumer<FileConfiguration> writer) {
+            super(fileName, path, writer);
+        }
+    }
 }
